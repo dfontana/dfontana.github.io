fd689da8febcc70a04a0fd0fd29c360bc41cd6b1
Dylan Fontana <dyfontana@gmail.com>
2017-05-15 14:10:38 -0400

    Filled out configuration in full

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
-if ! screen -list | grep -q "minecraft"; then
+if ! screen -list | grep -q "hugo"; then
     cd ~/website
-    screen -S hugo -d -m hugo server --port=8000 --baseUrl=192.168.0.16 --bind=192.168.0.16 --buildDrafts  
+    screen -S hugo -d -m hugo server --baseUrl=192.168.0.16 --bind=192.168.0.16 --buildDrafts  
 fi
 
